c92261f16246d7049f0ced367c63a8b9c2cda08f
Ujjwal Jain <ujjwaljain@Ujjwals-MacBook-Air.local>
2022-10-27 01:18:29 +0530

    issues resolved

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.use(
 app.get("/", (req, res) => {
   res.status(200);
 });
-app.use("/auth", auth);
-app.use("/events", events1);
+app.use("/api/auth", auth);
+app.use("/api/events", events1);
 // app.use("/api/events1", events1);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
