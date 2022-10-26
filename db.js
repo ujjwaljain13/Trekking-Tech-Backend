3ddcd4ce724561b70203a0f96fb53f1610c2285a
Ujjwal Jain <ujjwaljain@Ujjwals-MacBook-Air.local>
2022-10-26 19:06:57 +0530

    database changed

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const { Client } = require("pg");
-const client = new Client("postgres://weslkxke:7Udeo7VrnbTzCgWOStev8gRUBqppOQRL@arjuna.db.elephantsql.com/weslkxke");
+const client = new Client("postgres://ewqwnkze:4TEPuU0UqsE76NRFDxhc4zgew_U3G8Te@arjuna.db.elephantsql.com/ewqwnkze");
 
 module.exports = client;
