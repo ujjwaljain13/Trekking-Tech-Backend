0b2e368721d24df1b66e90efa446ef4808b2b4cb
Ujjwal Jain <ujjwaljain@Ujjwals-MacBook-Air.local>
2022-10-27 14:19:39 +0530

    chnaging variables

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = require("./db");
 const auth = require("./auth");
 const events1 = require("./events1");
 const cors = require("cors");
-const app = express();
+const server = express();
 client.connect((err) => {
   if (err) {
     console.error("connection error", err.stack);
@@ -13,24 +13,24 @@ client.connect((err) => {
 });
 
 const port = process.env.PORT || 5000;
-app.use(express.json());
-app.use(express.static("../frontend"));
+server.use(express.json());
+server.use(express.static("../frontend"));
 // const corsOptions = {
 //   origin: ['http://localhost:5000'],
 //   methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
 // };
 // http://127.0.0.1:5500/
-app.use(
+server.use(
   cors()
     // origin: "http://127.0.0.1:5500",
   
 );
-app.get("/", (req, res) => {
+server.get("/", (req, res) => {
   res.status(200);
 });
-app.use("/api/auth", auth);
-app.use("/api/events", events1);
+server.use("/api/auth", auth);
+server.use("/api/events", events1);
 // app.use("/api/events1", events1);
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
